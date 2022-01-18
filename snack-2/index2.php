da97774185a6d3ba4d90ebc4cd22d4ae7a91e268
AnaisDiGenua <anais.digenua5@gmail.com>
2022-01-18 17:29:03 +0100

    snack 2

--- snack-2/index2.php
+++ snack-2/index2.php
@@ -2,6 +2,14 @@
 
 <?php
 
+$name = $_GET["nome"];
+$mail = $_GET["mail"];
+$age = $_GET["eta"];
 
+if(strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age) !== false ) {
+    echo 'Accesso riuscito';
+} else {
+    echo 'Accesso negato';
+};
 
 ?>
